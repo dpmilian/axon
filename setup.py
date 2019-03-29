d56c7dafc52e7783536f2ef74ae30f6306367d84
dpmilian <d.perez@scrobotics.es>
2019-03-29 05:04:32 +0100

    modifications to setup.py, v0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="axon-dpmilian",
-    version="0.0.1",
+    version="0.0.2",
     author="Daniel Perez",
     author_email="dperez@scrobotics.es",
     description="hub with zmq suscriber broker with influxdb time logging backend",
